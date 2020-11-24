8d6f34238c1b5b125e3cc7b13622b030ed12bfc1
prashanth-sams <sams.prashanth@gmail.com>
2020-11-24 15:46:02 +0400

    added docker image for execution

---
+++ docker-executor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull prashanthsams/node-jake-runner:1.0
+
+docker run --name Test01 --rm -t -v $(pwd):/workspace/ \
+-u $(id -u ${USER}):$(id -g ${USER}) \
+prashanthsams/node-jake-runner:1.0 \
+-c "export HOME=/workspace && npm install && npm run webdriver-update && npm test"
